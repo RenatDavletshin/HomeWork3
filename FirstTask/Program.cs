1b7880f3510e501ec37c39af7c35f004581a9346
RenatDavletshin <immortal1307@gmail.com>
2022-10-26 23:29:53 +0500

    ThirdTaskEnd

--- FirstTask/Program.cs
+++ FirstTask/Program.cs
@@ -1,4 +1,4 @@
 ﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа.
 Console.WriteLine("Введите трехзначное число: ");
-int num = int.Parse(Console.ReadLine());
+int num = int.Parse(Console.ReadLine() ?? "0");
 Console.WriteLine(num / 10 % 10);
--- ThirdTask/obj/Debug/net6.0/ThirdTask.AssemblyInfo.cs
+++ ThirdTask/obj/Debug/net6.0/ThirdTask.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("FirstTask")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ThirdTask")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("FirstTask")]
-[assembly: System.Reflection.AssemblyTitleAttribute("FirstTask")]
+[assembly: System.Reflection.AssemblyProductAttribute("ThirdTask")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ThirdTask")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
