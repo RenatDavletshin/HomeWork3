4181550c35e5249c24d65b3fa2cfa2bd7388f93f
RenatDavletshin <immortal1307@gmail.com>
2022-10-26 23:54:51 +0500

    SecondTaskEnd

--- SecondTask/Program.cs
+++ SecondTask/Program.cs
@@ -1,3 +1,11 @@
 ﻿// Напишите программу, которая выводит третью цифру заданного числа и сообщает, что третьей цифры нет.
 Console.WriteLine("Введите число: ");
-
+int num = int.Parse(Console.ReadLine() ?? "0");
+if(num > 99)
+{
+    Console.WriteLine(num.ToString()[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
